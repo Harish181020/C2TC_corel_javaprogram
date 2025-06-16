def4101e22c8bfffbc979606e92dfa8a42d816ee
ACT-C2-23 <ACT-C2-23@DESKTOP-I4SCAC5>
2025-06-16 14:59:58 +0530

    update

---
+++ c2tc_corejava_program/src/day3/Encapsulationdemo.java
@@ -0,0 +1,18 @@
+package day3;
+
+public class Encapsulationdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//object creation 
+				OopsConceptDemo obj = new OopsConceptDemo();
+				obj.setSerialNum(101);
+				obj.setName("Pooja");
+				obj.setAge(20);
+				System.out.println(obj);
+				
+			}
+
+	}
+
+
