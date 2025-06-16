8c9e8d4920e91d46a0250723c30c2a8191f6889c
ACT-C2-23 <ACT-C2-23@DESKTOP-I4SCAC5>
2025-06-16 14:13:00 +0530

    update

---
+++ c2tc_corejava_program/src/day2/Breakdemo.java
@@ -0,0 +1,17 @@
+package day2;
+
+public class Breakdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 9; i < 10; i++) {
+			if (i == 5)
+				break;
+			System.out.println(i);
+		}
+
+	}
+
+	}
+
+
---
+++ c2tc_corejava_program/src/day2/third.java
@@ -0,0 +1,15 @@
+package day2;
+
+public class third {
+
+	public static void main(String[] args) {
+		int i = 11;
+		do {
+			System.out.println(i);
+			i++;
+		} while (i <= 1);
+
+
+	}
+
+}
