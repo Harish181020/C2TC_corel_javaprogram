002031a6914ebe0758e1b22e16031399f178e4eb
ACT-C2-23 <ACT-C2-23@DESKTOP-I4SCAC5>
2025-07-02 09:59:32 +0530

    update

---
+++ c2tc_corejava_program/src/exceptionalhandling/ExceptionExample.java
@@ -0,0 +1,16 @@
+package exceptionalhandling;
+
+public class ExceptionExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+      int a[]= {6,7,8,4};
+      System.out.println(a[5]);
+	}
+	catch(ArrayIndexOutOfBoundsExpection ae) {
+	}
+		System.err.println("Exception occurs"+a.get)
+		System.out.println("next line");
+}
+}
